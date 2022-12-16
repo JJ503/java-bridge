9ea6de60bfdd2bf5538d8c7bfc31854661caf674
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 20:45:09 +0900

    feat(BridgeGameController): 다리 길이 입력 받기 연결 및 생성 기능 추가
    docs: 다리 길이 입력 받기 연결, 다리 생성 완료 체크

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,7 +1,12 @@
 package bridge.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
+import bridge.model.Bridge;
 import bridge.view.InputView;
 import bridge.view.OutputView;
+
+import java.util.List;
 
 public class BridgeGameController {
     InputView inputView = new InputView();
@@ -9,5 +14,21 @@ public class BridgeGameController {
 
     public void startBridgeGame() {
         outputView.printStartGameMessage();
+        Bridge bridge = initBridge();
+    }
+
+    private Bridge initBridge() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        List<String> bridge = bridgeMaker.makeBridge(enterBridgeSize());
+        return new Bridge(bridge);
+    }
+
+    private Integer enterBridgeSize() {
+        try {
+            return inputView.readBridgeSize();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return enterBridgeSize();
+        }
     }
 }
