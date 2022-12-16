0a087de166cdbd755f3388b0240f0b855d939608
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 22:54:29 +0900

    feat(Reply): 재시도 여부에 따른 boolean 값 추가

--- src/main/java/bridge/constants/Reply.java
+++ src/main/java/bridge/constants/Reply.java
@@ -6,23 +6,29 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Reply {
-    RETRY("R"),
-    STOP("Q");
+    RETRY("R", true),
+    STOP("Q", false);
 
-    private static final Map<String, Reply> RETRY_BY_MARK = Stream.of(values())
-            .collect(Collectors.toMap(Reply::getReply, Function.identity()));
+    private static final Map<String, Boolean> RETRY_BY_MARK = Stream.of(values())
+            .collect(Collectors.toMap(Reply::getReply, Reply::getReplyState));
 
     private final String reply;
+    private final boolean retryState;
 
-    Reply(String reply) {
+    Reply(String reply, boolean retryState) {
         this.reply = reply;
+        this.retryState = retryState;
     }
 
     private String getReply() {
         return reply;
     }
 
-    private static Reply getReply(String reply) {
+    private boolean getReplyState() {
+        return retryState;
+    }
+
+    private static boolean getReply(String reply) {
         return RETRY_BY_MARK.get(reply);
     }
 }
