55a34cc74334c0f6936cffc224f6933ba398adeb
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 23:14:07 +0900

    style(InputView): 입력시 출력 메시지 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_BRIDGE_SIZE = "\n다리의 길이를 입력해주세요.";
+    private static final String INPUT_POSITION = "\n이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_REPLY = "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     public int readBridgeSize() {
         System.out.println(INPUT_BRIDGE_SIZE);
@@ -12,6 +14,7 @@ public class InputView {
     }
 
     public String readMoving() {
+        System.out.println(INPUT_POSITION);
         return InputValidation.validateReply(Console.readLine());
     }
 
@@ -19,6 +22,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println(INPUT_REPLY);
         return InputValidation.validateReply(Console.readLine());
     }
 }
