407b56640f7e2e1bb4cd2ad799e75d334c79a0b8
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 22:37:04 +0900

    feat(Grades): 정답 여부 리스트를 저장하고 관리하는 기능 추가
    docs: 정답 리스트 초기화, 정답 추가, 정답 리스트 반환하기 완료 체크

---
+++ src/main/java/bridge/model/Grades.java
@@ -0,0 +1,17 @@
+package bridge.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Grades {
+    private final List<Boolean> grades = new ArrayList<>();
+
+    public void addGrade(Boolean grade) {
+        grades.add(grade);
+    }
+
+    public List<Boolean> getGrades() {
+        return Collections.unmodifiableList(grades);
+    }
+}
