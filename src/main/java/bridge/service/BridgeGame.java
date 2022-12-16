a989e8b8cba7e45a5faee49d3471ee170274f74e
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 22:39:48 +0900

    feat(BridgeGame): 다리 결과 정답 리스트에 추가 기능 추가
    docs: 다리 결과 정답 리스트에 추가 완료 체크

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,7 +1,11 @@
 package bridge.service;
 
 import bridge.model.Bridge;
+import bridge.model.Grades;
 import bridge.model.TryCount;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -9,10 +13,12 @@ import bridge.model.TryCount;
 public class BridgeGame {
     private final Bridge bridge;
     private final Bridge userBridge;
+    private final Grades grades;
 
     public BridgeGame(Bridge bridge) {
         this.bridge = bridge;
         this.userBridge = new Bridge();
+        this.grades = new Grades();
     }
 
     /**
@@ -20,10 +26,16 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move(String position) {
+    public boolean move(String position) {
         userBridge.addPosition(position);
         int lastIndex = userBridge.getBridgeSize() - 1;
-        bridge.isMatchPosition(userBridge.getPositionByIndex(lastIndex), lastIndex);
+        boolean grade = bridge.isMatchPosition(userBridge.getPositionByIndex(lastIndex), lastIndex);
+        grades.addGrade(grade);
+        return grade;
+    }
+
+    public void getMap() {
+
     }
 
     /**
