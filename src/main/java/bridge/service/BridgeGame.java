8ed477c34a885979c851c7fc73f4231e9b5bfb98
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 23:03:23 +0900

    feat(BridgeGame): 다리 결과 반환하기 기능 추가
    docs: 다리 결과 반환하기 완료 체크

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -1,11 +1,8 @@
 package bridge.service;
 
+import bridge.constants.BridgeDraw;
 import bridge.model.Bridge;
 import bridge.model.Grades;
-import bridge.model.TryCount;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -21,11 +18,6 @@ public class BridgeGame {
         this.grades = new Grades();
     }
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     public boolean move(String position) {
         userBridge.addPosition(position);
         int lastIndex = userBridge.getBridgeSize() - 1;
@@ -34,8 +26,8 @@ public class BridgeGame {
         return grade;
     }
 
-    public void getMap() {
-
+    public String getMap() {
+        return BridgeDraw.getBridgeDraw(userBridge.getBridge(), grades.getGrades());
     }
 
     /**
