ce829db983cd2dedbe6962a23d0f0261986ea54e
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 21:01:24 +0900

    feat(InputView): 이동 칸, 재시도 여부 답 입력 및 검증 연결 기능 추가
    docd: 이동 칸, 재시도 여부 답 입력 및 검증 연결 완료 체크

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -11,17 +11,14 @@ public class InputView {
         return InputValidation.validateBridgeSize(Console.readLine());
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        return null;
+        return InputValidation.validateReply(Console.readLine());
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        return InputValidation.validateReply(Console.readLine());
     }
 }
