c651e0b132ef675c7a6d9c16730927e8a956eaf7
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 20:20:43 +0900

    feat(InputValidation): 3이상 20이하의 값인지 확인 기능 추가
    docs: 3이상 20이하의 값인지 확우 완료 체크

--- src/main/java/bridge/validation/InputValidation.java
+++ src/main/java/bridge/validation/InputValidation.java
@@ -3,10 +3,14 @@ package bridge.validation;
 import bridge.constants.ExceptionMessage;
 
 public class InputValidation {
+    private static final int MINIMUM_BRIDGE_SIZE = 3;
+    private static final int MAXIMUM_BRIDGE_SIZE = 20;
 
     public static Integer validateBridgeSize(String bridgeSize) {
         isExist(bridgeSize);
-        return toNumber(bridgeSize);
+        Integer sizeNumber = toNumber(bridgeSize);
+        isBetween3And20(sizeNumber);
+        return sizeNumber;
     }
 
     private static void isExist(String inputValue) {
@@ -23,5 +27,16 @@ public class InputValidation {
         }
 
         return null;
+    }
+
+    private static void isBetween3And20(int checkSize) {
+        if (!checkBetween3And20(checkSize)) {
+            ExceptionMessage.NOT_INPUT_NAME_BETWEEN_3AND20.throwException();
+        }
+    }
+
+    // 조건에 맞는지 확인
+    private static boolean checkBetween3And20(int checkSize) {
+        return MINIMUM_BRIDGE_SIZE <= checkSize && checkSize < MAXIMUM_BRIDGE_SIZE;
     }
 }
