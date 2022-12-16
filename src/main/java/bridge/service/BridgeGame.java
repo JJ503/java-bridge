7f3e84e5ed5c8a6df0c3b5e37f4952ddf3857f5a
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 00:14:05 +0900

    feat(BridgeDraw): 다리 건너기 완료시 성공 여부 체크 기능 추가

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -11,20 +11,30 @@ public class BridgeGame {
     private final Bridge bridge;
     private final Bridge userBridge;
     private final Grades grades;
+    private boolean isSuccess;
 
     public BridgeGame(Bridge bridge) {
         this.bridge = bridge;
         this.userBridge = new Bridge();
         this.grades = new Grades();
+        this.isSuccess = false;
     }
 
     public boolean move(String position) {
+        boolean moveState = moveEvent(position);
+        isSuccess = checkBridgeState();
+        System.out.println(isSuccess);
+        return moveState && !isOverToMakeBridge();
+    }
+
+    private boolean moveEvent(String position) {
         userBridge.addPosition(position);
         int lastIndex = userBridge.getBridgeSize() - 1;
         boolean grade = bridge.isMatchPosition(userBridge.getPositionByIndex(lastIndex), lastIndex);
         grades.addGrade(grade);
         return grade;
     }
+
 
     public String getMap() {
         return BridgeDraw.getBridgeDraw(userBridge.getBridge(), grades.getGrades());
@@ -38,5 +48,25 @@ public class BridgeGame {
     public void retry() {
         userBridge.resetBridge();
         grades.resetGrades();
+    }
+
+    public boolean getIsSuccess() {
+        return isSuccess;
+    }
+
+    private boolean checkBridgeState() {
+        if (isOverToMakeBridge()) {
+            return isSuccess();
+        }
+
+        return false;
+    }
+
+    private boolean isOverToMakeBridge() {
+        return bridge.isSameSize(userBridge.getBridgeSize());
+    }
+
+    private boolean isSuccess() {
+        return bridge.isAllCorrect(userBridge.getBridge());
     }
 }
