e6ed07d9d0c73ff85d544e6a2de65575fcf22925
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-16 20:20:04 +0900

    feat(ExceptionMessage): 에러 문구 상수 추가
    - 다리 크기가 3에서 20 사이의 값이 아닌 경우

--- src/main/java/bridge/constants/ExceptionMessage.java
+++ src/main/java/bridge/constants/ExceptionMessage.java
@@ -2,7 +2,8 @@ package bridge.constants;
 
 public enum ExceptionMessage {
     NONE_INPUT("값을 입력해 주셔야 합니다."),
-    NOT_INPUT_NUMBER("숫자를 입력해 주셔야 합니다.");
+    NOT_INPUT_NUMBER("숫자를 입력해 주셔야 합니다."),
+    NOT_INPUT_NAME_BETWEEN_3AND20("3이상 20이하의 숫자를 입력해 주셔야 합니다.");
 
     private static final String ERROR_TAG = "[ERROR] ";
     private final String exceptionMessage;
